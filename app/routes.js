14821c4eb54b835ee3dd6e18eac6de2f117fc442
Destan Sarpkaya <destan@kodgemisi.com>
2016-10-16 04:39:04 +0300

    add bradcrumb & blank dictionaries page

--- app/routes.js
+++ app/routes.js
@@ -5,11 +5,13 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 import Dashboard from './containers/Dashboard';
+import Dictionaries from './containers/Dictionaries';
 
 
 export default (
   <Route name="Dashboard" path="/" component={App}>
     <IndexRoute component={Dashboard} />
     <Route name="Counter" path="/counter" component={CounterPage} />
+    <Route name="Dictionaries" path="/dictionaries" component={Dictionaries} />
   </Route>
 );
